16fa85d15c492658bff38b079dc96397346871b9
Redstoney96 <140266332+Redstoney96@users.noreply.github.com>
2025-06-29 14:57:50 +0200

    Main Program

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,246 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+
+namespace Overview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private string[] config;
+
+        private string app1Path = "notepad.exe";
+        private string app2Path = "notepad.exe";
+        private string app3Path = "notepad.exe";
+        private string app4Path = "notepad.exe";
+
+        private string folder1Path = "explorer.exe";
+        private string folder2Path = "explorer.exe";
+        private string folder3Path = "explorer.exe";
+        private string folder4Path = "explorer.exe";
+        private string folder5Path = "explorer.exe";
+        private string folder6Path = "explorer.exe";
+
+        private string folder1Text = "";
+        private string folder2Text = "";
+        private string folder3Text = "";
+        private string folder4Text = "";
+        private string folder5Text = "";
+        private string folder6Text = "";
+
+        private BitmapImage appPlaceholderIcon = new BitmapImage(new Uri("C:\\Users\\Youtube\\Pictures\\placeholdericon2.png"));
+        private BitmapImage appIcon;
+        public MainWindow()
+        {
+            InitializeComponent();
+            loadingAnimation();
+            try
+            {
+                config = File.ReadAllLines("C:\\Overview\\Config.yml");
+            }
+            catch
+            {
+                File.WriteAllLines("C:\\Overview\\Config.yml", Enumerable.Range(1, 35).Select(n => n.ToString()));
+                config = File.ReadAllLines("C:\\Overview\\Config.yml");
+            }
+
+            try
+            {
+                if (config[0] != "1")
+                {
+                    app1Path = config[0];
+                }
+                if (config[1] != "2")
+                {
+                    app2Path = config[1];
+                }
+                if (config[2] != "3")
+                {
+                    app3Path = config[2];
+                }
+                if (config[3] != "4")
+                {
+                    app4Path = config[3];
+                }
+            }
+            catch
+            {
+
+            }
+
+            try
+            {
+                if (config[4] != "5")
+                {
+                    app1.Source = new BitmapImage(new Uri(config[4]));
+                }
+                if (config[5] != "6")
+                {
+                    app2.Source = new BitmapImage(new Uri(config[5]));
+                }
+                if (config[6] != "7")
+                {
+                    app3.Source = new BitmapImage(new Uri(config[6]));
+                }
+                if (config[7] != "8")
+                {
+                    app4.Source = new BitmapImage(new Uri(config[7]));
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Icon Error");
+            }
+
+            try
+            {
+                if (Directory.Exists(config[8]))
+                {
+                    folder1Path = config[8];
+                    folder1T.Content = System.IO.Path.GetFileName(config[8]);
+                }
+                else
+                {
+                    folder1T.Content = "----------";
+                }
+
+                if (Directory.Exists(config[9]))
+                {
+                    folder2Path = config[9];
+                    folder2T.Content = System.IO.Path.GetFileName(config[9]);
+                }
+                else
+                {
+                    folder2T.Content = "----------";
+                }
+
+                if (Directory.Exists(config[10]))
+                {
+                    folder3Path = config[10];
+                    folder3T.Content = System.IO.Path.GetFileName(config[10]);
+                }
+                else
+                {
+                    folder3T.Content = "----------";
+                }
+
+                if (Directory.Exists(config[11]))
+                {
+                    folder4Path = config[11];
+                    folder4T.Content = System.IO.Path.GetFileName(config[11]);
+                }
+                else
+                {
+                    folder4T.Content = "----------";
+                }
+
+                if (Directory.Exists(config[12]))
+                {
+                    folder5Path = config[12];
+                    folder5T.Content = System.IO.Path.GetFileName(config[12]);
+                }
+                else
+                {
+                    folder5T.Content = "----------";
+                }
+
+                if (Directory.Exists(config[13]))
+                {
+                    folder6Path = config[13];
+                    folder6T.Content = System.IO.Path.GetFileName(config[13]);
+                }
+                else
+                {
+                    folder6T.Content = "----------";
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Config Error");
+            }
+
+
+
+        }
+
+        private async void loadingAnimation()
+        {
+            for (int i = -30; i <= 10; i++)
+            {
+                mainPanel.Margin = new Thickness(i, i, i, i);
+                await Task.Delay(1);
+            }
+        }
+        private void app1Click(object sender, MouseEventArgs e)
+        {
+            Process.Start(app1Path);
+        }
+        private void app2Click(object sender, MouseEventArgs e)
+        {
+            Process.Start(app2Path);
+        }
+        private void app3Click(object sender, MouseEventArgs e)
+        {
+            Process.Start(app3Path);
+        }
+        private void app4Click(object sender, MouseEventArgs e)
+        {
+            Process.Start(app4Path);
+        }
+
+
+        // Folder Logic
+        private void folder1Click(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("explorer.exe", folder1Path);
+        }
+        private void folder2Click(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("explorer.exe", folder2Path);
+        }
+        private void folder3Click(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("explorer.exe", folder3Path);
+        }
+        private void folder4Click(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("explorer.exe", folder4Path);
+        }
+        private void folder5Click(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("explorer.exe", folder5Path);
+        }
+        private void folder6Click(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("explorer.exe", folder6Path);
+        }
+        // End of Folder Logic
+        // Open Settings
+        private void noteCreate(object sender, RoutedEventArgs e)
+        {
+            //File.WriteAllText("C:\\Overview\\Notes.txt", noteText.Text);
+            //MessageBox.Show("Note was saved", "Notes");
+        }
+
+        private void openSettings(object sender, RoutedEventArgs e)
+        {
+            var settingsWindow = new SettingsWindow();
+            settingsWindow.Show();
+        }
+    }
+}
---
+++ SettingsWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Overview
+{
+    /// <summary>
+    /// Interaktionslogik für SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+            loadingAnimation();
+        }
+
+        private void autoStartOn(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Auto Start is enabled", "Settings");
+        }
+        private void autoStartOff(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Auto Start is disable", "Settings");
+        }
+        private async void loadingAnimation()
+        {
+            for (int i = -30; i <= 10; i++)
+            {
+                mainPanel.Margin = new Thickness(i, i, i, i);
+                await Task.Delay(1);
+            }
+        }
+    }
+}
